837149128158dc864fe8a5f5a6ec7d5f9827b4a6
Jose Luis Vazquez Gonzalez <josvaz@vmware.com>
2021-10-27 16:27:11 +0200

    Document auth adding links to external docs
    Signed-off-by: Jose Luis Vazquez Gonzalez <josvaz@vmware.com>

--- pkg/mover/auth.go
+++ pkg/mover/auth.go
@@ -7,6 +7,13 @@ import "github.com/google/go-containerregistry/pkg/authn"
 
 type authnKeychain Repository
 
+// Resolve implements an authn.KeyChain
+//
+// See https://pkg.go.dev/github.com/google/go-containerregistry/pkg/authn#Keychain
+//
+// Returns a custom credentials authn.Authenticator if the given resource
+// RegistryStr() matches the Repository, otherwise it falls back to the default
+// KeyChain which may include local docker credentials.
 func (kc authnKeychain) Resolve(resource authn.Resource) (authn.Authenticator, error) {
 	if kc.Server == resource.RegistryStr() {
 		return &authenticator{Repository(kc)}, nil
@@ -18,6 +25,11 @@ type authenticator struct {
 	Repository
 }
 
+// Authorization implements an authn.Authenticator
+//
+// See https://pkg.go.dev/github.com/google/go-containerregistry/pkg/authn#Authenticator
+//
+// Returns an authn.AuthConfig with a user / password pair to be used for authentication
 func (auth *authenticator) Authorization() (*authn.AuthConfig, error) {
 	return &authn.AuthConfig{Username: auth.Username, Password: auth.Password}, nil
 }
