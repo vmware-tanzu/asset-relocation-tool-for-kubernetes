bb5a04b7ed07f2ce26eef4ab262c5c4217ca82c8
josvaz <josvaz@vmware.com>
2021-11-23 15:52:19 +0100

    Update cmd/chart.go
    Make flag description lower case
    
    Co-authored-by: Miguel Martínez Triviño <mtrivino@vmware.com>
    Signed-off-by: Jose Luis Vazquez Gonzalez <josvaz@vmware.com>

--- cmd/chart.go
+++ cmd/chart.go
@@ -78,7 +78,7 @@ func newChartMoveCmd() *cobra.Command {
 	f.UintVar(&retries, "retries", defaultRetries, "number of times to retry push operations")
 	f.StringVar(&output, "out", "*.relocated.tgz", "name of the resulting chart")
 
-	f.StringVar(&toArchive, "to-archive", "", "Save the chart and all its dependencies to and offline archive tarball")
+	f.StringVar(&toArchive, "to-archive", "", "save the chart and all its dependencies to and offline archive tarball")
 
 	return cmd
 }
