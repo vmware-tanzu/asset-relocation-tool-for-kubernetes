3848063a3a782d34123b5961823695137d5037aa
Miguel Martinez Trivino <mtrivino@vmware.com>
2022-01-18 12:12:13 +0100

    feat: container keychains exclusion
    Signed-off-by: Miguel Martinez Trivino <mtrivino@vmware.com>

--- pkg/mover/auth.go
+++ pkg/mover/auth.go
@@ -10,13 +10,14 @@ import "github.com/google/go-containerregistry/pkg/authn"
 // See https://pkg.go.dev/github.com/google/go-containerregistry/pkg/authn#Keychain
 //
 // Returns a custom credentials authn.Authenticator if the given resource
-// RegistryStr() matches the Repository, otherwise it falls back to the default
-// KeyChain which may include local docker credentials.
+// RegistryStr() matches the Repository, otherwise it returns annonymous access
 func (repo ContainerRepository) Resolve(resource authn.Resource) (authn.Authenticator, error) {
 	if repo.Server == resource.RegistryStr() {
 		return repo, nil
 	}
-	return authn.DefaultKeychain.Resolve(resource)
+
+	// if no credentials are provided we return annon authentication
+	return authn.Anonymous, nil
 }
 
 // Authorization implements an authn.Authenticator
@@ -26,4 +27,14 @@ func (repo ContainerRepository) Resolve(resource authn.Resource) (authn.Authenti
 // Returns an authn.AuthConfig with a user / password pair to be used for authentication
 func (repo ContainerRepository) Authorization() (*authn.AuthConfig, error) {
 	return &authn.AuthConfig{Username: repo.Username, Password: repo.Password}, nil
+}
+
+// Define a container images keychain based on the settings provided via the containers struct
+// If useDefaultKeychain is set, use config/docker.json otherwise load the provided creds (if any)
+func getContainersKeychain(c Containers) authn.Keychain {
+	if c.UseDefaultLocalKeychain {
+		return authn.DefaultKeychain
+	}
+
+	return c.ContainerRepository
 }
